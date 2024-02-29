4b751c86607f86136d500c622e7c63d826829705
tatheer.fatima <tatheer@codesorbit.com>
2024-02-29 15:05:46 +0500

    push code

--- app/src/main/java/com/example/anew/MainActivity.kt
+++ app/src/main/java/com/example/anew/MainActivity.kt
@@ -2,12 +2,9 @@ package com.example.anew
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.jenkins_cicd.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val content = ""
     }
 }
